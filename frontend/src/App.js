fd8a1da4f93a2c9fc667a1da3b848bc35910aae4
valerias-mr <valeria.roldan1358@gmail.com>
2022-10-28 17:20:24 -0500

    creacion componente de ruta protegida

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,29 @@ import Login from './pages/Login';
 import Inicial from './pages/Inicial';
 import Register from './pages/Register';
 import Asientos from './pages/Asientos';
+import ProtectedRoute from './components/ProtectedRoute';
 import './css/App.css'
 
 function App() {
   return (
     <div className="min-h-screen bg-primary overflow">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-     <div className="">
-     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Inicial/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register'element={<Register/>} />
-        <Route path='/asientos'element={<Asientos/>} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-    </div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className="">
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={
+                <ProtectedRoute>
+                  <Asientos />
+                </ProtectedRoute>
+              } />
+              <Route path='/' element={<Inicial />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/asientos' element={<Asientos />} /> 
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </div>
   );
 }
