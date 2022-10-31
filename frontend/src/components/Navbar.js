5c75087f055247cde8ba3f9233f113403777494b
valerias-mr <valeria.roldan1358@gmail.com>
2022-10-31 13:22:04 -0500

    Coreccion boton logOut

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles";
 import { logo } from "../assets";
 import { navLinks } from "../constants";
@@ -7,13 +7,18 @@ import { navLinks } from "../constants";
 
 const Navbar = () => {
   const [active, setActive] = useState("Home");
-  //const [botonActivo, setBotonActivo] = useState(false);
+  const currentUser = localStorage.getItem('Id')
+  const [botonActivo, setBotonActivo] = useState(true);
   function logout() {
     localStorage.clear();
-    window.location.href = "/";
-    //setBotonActivo("hidden");
-    //llmara funciones btn
+    setBotonActivo(false);
+    console.log(botonActivo)
   }
+  useEffect(() => {
+    if(!currentUser){
+      setBotonActivo(false)
+    }
+  }, [currentUser])  
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
@@ -23,14 +28,14 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
+              } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
             onClick={() => setActive(nav.title)}
           >
             <button
               onClick={logout}
               type="button"
+              hidden={!botonActivo}
               className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
             >
               Salir
