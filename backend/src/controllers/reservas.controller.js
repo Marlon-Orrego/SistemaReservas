7458d889c1f0ffbf09b62111a1bc12149cabe7c0
Simon <londonosimon67873@gmail.com>
2022-11-02 09:19:06 -0500

    Correción Back Reservas

--- backend/src/controllers/reservas.controller.js
+++ backend/src/controllers/reservas.controller.js
@@ -18,7 +18,7 @@ export const getReservabyId = async (req, res) => {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input("Id", req.params.Id)
+      .input("Id_Reserva", req.params.Id)
       .query(queriesReservas.getReservabyId);
     return res.json(result.recordset[0]);
   } catch (error) {
@@ -32,7 +32,7 @@ export const deleteReserva = async (req, res) => {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input("Id", req.params.Id)
+      .input("Id_Reserva", req.params.Id)
       .query(queriesReservas.deleteReserva);
     if (result.rowsAffected[0] === 0) return res.sendStatus(404);
     return res.sendStatus(204);
@@ -43,10 +43,10 @@ export const deleteReserva = async (req, res) => {
 };
 
 export const updateReserva = async (req, res) => {
-  const { Nombre, correo, Id, contraseña } = req.body;
+  const { Id_Reserva, Id_Vuelo, Id_Cliente, Precio } = req.body;
 
   // validating
-  if (Id == null || Nombre == null || correo == null || contraseña == null) {
+  if (Id == null || Id_Reserva == null || Id_Vuelo == null || Id_Cliente == null|| Precio == null) {
     return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
   }
 
@@ -54,12 +54,13 @@ export const updateReserva = async (req, res) => {
     const pool = await getConnection();
     await pool
       .request()
-      .input("Nombre", sql.VarChar, Nombre)
-      .input("correo", sql.VarChar, correo)
-      .input("contraseña", sql.VarChar, contraseña)
+      .input("Id_Reserva", sql.VarChar, Id_Reserva)
+      .input("Id_Vuelo", sql.VarChar, Id_Vuelo)
+      .input("Precio", sql.Int, Precio)
+      .input("Id_Cliente", sql.VarChar, Id_Cliente)
       .input("Id", req.params.Id)
       .query(queriesReservas.updateReserva);
-    res.json({ Nombre, correo, contraseña, Id });
+    res.json({ Id_Reserva, Id_Vuelo, Id_Cliente, Precio });
   } catch (error) {
     res.status(500);
     res.send(error.message);
@@ -67,9 +68,10 @@ export const updateReserva = async (req, res) => {
 };
 
 export const createReserva = async (req, res) => {
-  const { Nombre, Id, correo, contraseña } = req.body;
+  const { Id_Reserva, Id_Vuelo, Id_Cliente, Precio } = req.body;
+
   // validating
-  if (Id == null || Nombre == null || correo == null || contraseña == null) {
+  if (Id == null || Id_Reserva == null || Id_Vuelo == null || Id_Cliente == null|| Precio == null) {
     return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
   }
 
@@ -77,12 +79,12 @@ export const createReserva = async (req, res) => {
     const pool = await getConnection();
     await pool
       .request()
-      .input("Id", sql.Int, Id)
-      .input("Nombre", sql.VarChar, Nombre)
-      .input("correo", sql.VarChar, correo)
-      .input("contraseña", sql.VarChar, contraseña)
+      .input("Id_Reserva", sql.VarChar, Id_Reserva)
+      .input("Id_Vuelo", sql.VarChar, Id_Vuelo)
+      .input("Id_Cliente", sql.VarChar, Id_Cliente)
+      .input("Precio", sql.Int, Precio)
       .query(queriesReservas.createReserva);
-    res.json({ Nombre, correo, contraseña, Id });
+    res.json({ Id_Reserva, Id_Vuelo, Id_Cliente, Precio });
   } catch (error) {
     res.status(500);
     res.send(error.message);
